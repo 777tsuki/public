3a4c9808c41b7f8b7311dff74f908d91b6a0aef5
和祯_BT <0@7tsuki.fun>
2023-09-20 23:46:37 +0800

    整合弹幕小游戏

---
+++ public/js/da-style.js
@@ -0,0 +1,86 @@
+const style = {
+  first: {
+    box: {
+      shape: 'oval',
+      a: 14,
+      b: 7,
+      size: 28
+    },
+    rate: 300,
+    speed: 5,
+    design: ''
+  },
+  six: {
+    box: {
+      shape: 'oval',
+      a: 14,
+      b: 7,
+      size: 28
+    },
+    speed: 5,
+    num: 5,
+    rate: 70,
+    rotate: Math.PI / 17,
+    design: ''
+  },
+  seven: {
+    box: {
+      shape: 'oval',
+      a: 14,
+      b: 7,
+      size: 28
+    },
+    speed: 5,
+    num: 5,
+    rate: 50,
+    rotate: -Math.PI / 17,
+    design: ''
+  },
+  eight: {
+    box: {
+      shape: 'round',
+      size: 14
+    },
+    speed: 3,
+    num: 5,
+    rate: 150,
+    translate: -Math.PI / 89,
+    rotate: -Math.PI / 11,
+    design: ''
+  },
+  nine: {
+    box: {
+      shape: 'oval',
+      a: 14,
+      b: 7,
+      size: 28
+    },
+    offset: {
+      x: -120
+    },
+    speed: 5,
+    num: 6,
+    rate: 100,
+    rotate: Math.PI / 11,
+    design: ''
+  },
+  ten: {
+    box: {
+      shape: 'oval',
+      a: 14,
+      b: 7,
+      size: 28
+    },
+    offset: {
+      x: 120
+    },
+    speed: 4,
+    num: 5,
+    rate: 80,
+    rotate: -Math.PI / 17,
+    design: ''
+  },
+  walk: {
+    speed: 0.7
+  }
+}
